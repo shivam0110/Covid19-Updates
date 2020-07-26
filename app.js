52ac22df162c83a1aee213690993180749147209
Shivam <54373797+shivam0110@users.noreply.github.com>
2020-07-26 11:32:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,29 +10,38 @@ function commas(x) {
 
 
 const proxy = "https://cors-anywhere.herokuapp.com/";
-const api1 = `https://api.covid19api.com/summary`;
+const api1 = `https://api.thevirustracker.com/free-api?global=stats`;
 fetch(api1)
     .then(response =>{
         return response.json();
     })
     .then(data=>{
-        //console.log(data);
-        document.getElementById('gtc2').textContent = commas(data.Global.TotalConfirmed);
-        document.getElementById('gtc3').textContent = '+' + commas(data.Global.NewConfirmed);
-        document.getElementById('gd2').textContent = commas(data.Global.TotalDeaths);
-        document.getElementById('gd3').textContent = '+' + commas(data.Global.NewDeaths);
-        document.getElementById('gac2').textContent = commas((data.Global.TotalConfirmed - data.Global.TotalDeaths - data.Global.TotalRecovered));        
-        document.getElementById('grc2').textContent = commas(data.Global.TotalRecovered);        
-        document.getElementById('grc3').textContent = '+' + commas(data.Global.NewRecovered);        
-        document.getElementById('gcc2').textContent = commas((data.Global.TotalDeaths+data.Global.TotalRecovered));        
-        document.getElementById('gcc3').textContent = '+' + commas((data.Global.NewDeaths+data.Global.NewRecovered));    
         
-        if((data.Global.NewConfirmed - data.Global.NewDeaths - data.Global.NewRecovered)>=0){
+        console.log(data);
+        document.getElementById('gtc2').textContent = commas(data.results[0].total_cases);
+        document.getElementById('gd2').textContent = commas(data.results[0].total_deaths);
+        document.getElementById('grc2').textContent = commas(data.results[0].total_recovered);        
+        document.getElementById('gcc2').textContent = commas((data.results[0].total_deaths + data.results[0].total_recovered));        
+        document.getElementById('gac2').textContent = commas((data.results[0].total_cases - data.results[0].total_deaths - data.results[0].total_recovered));        
+
+        api6 = `https://api.covid19api.com/summary`;
+        fetch(api6)
+        .then(response =>{
+            return response.json();
+        })
+        .then(data=>{
+            document.getElementById('gtc3').textContent = '+' + commas(data.Global.NewConfirmed);
+            document.getElementById('gd3').textContent = '+' + commas(data.Global.NewDeaths);
+            document.getElementById('grc3').textContent = '+' + commas(data.Global.NewRecovered);        
+            document.getElementById('gcc3').textContent = '+' + commas((data.Global.NewDeaths+data.Global.NewRecovered));    
+            if((data.Global.NewConfirmed - data.Global.NewDeaths - data.Global.NewRecovered)>=0){
             document.getElementById('gac3').textContent = '+' + commas((data.Global.NewConfirmed - data.Global.NewDeaths - data.Global.NewRecovered));        
-        }
-        else{  
-            document.getElementById('gac3').textContent = commas((data.Global.NewConfirmed - data.Global.NewDeaths - data.Global.NewRecovered));        
-        }
+            }
+            else{  
+                document.getElementById('gac3').textContent = commas((data.Global.NewConfirmed - data.Global.NewDeaths - data.Global.NewRecovered));        
+            }
+        
+        });
 });   
 
 

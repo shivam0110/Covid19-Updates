83f2932a71f2348693748ec4335b7743d523ab7b
Shivam <54373797+shivam0110@users.noreply.github.com>
2020-08-03 21:55:46 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@ let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
 navBarToggle.addEventListener('click', function () {
-    
     mainNav.classList.toggle('active');
 });
+
+function toggle(){
+    mainNav.classList.toggle('active');
+}

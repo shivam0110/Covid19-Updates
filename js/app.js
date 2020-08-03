6a2f0ff4515e8b6b5ef6415dc484c1185fad9fb2
Shivam <54373797+shivam0110@users.noreply.github.com>
2020-08-03 23:33:31 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//Navbar
 let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
@@ -7,4 +8,25 @@ navBarToggle.addEventListener('click', function () {
 
 function toggle(){
     mainNav.classList.toggle('active');
+}
+
+//Info
+
+function info() {
+    var x = document.getElementById("information");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+//News
+function news() {
+    var x = document.getElementById("news-section");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
 }
